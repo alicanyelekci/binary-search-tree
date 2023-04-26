175259ddf889b30676b4e139da3809290096fad0
Ali Can Yelekci <acyelekci@gmail.com>
2023-04-26 17:50:40 +0300

    printing balanced BST from unsorted array

---
+++ BST.js
@@ -0,0 +1,80 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.leftNode = null;
+    this.rightNode = null;
+  }
+}
+
+class Tree {
+  constructor(arr) {
+    arr = mergeSort(arr);
+    arr = removeSame(arr);
+    this.arr = arr;
+    this.root = buildTree(arr);
+  }
+}
+
+const buildTree = (arr) => {
+  if (arr.length === 0) {
+    return null;
+  }
+  const mid = Math.floor(arr.length / 2);
+  const node = new Node(arr[mid]);
+  node.leftNode = buildTree(arr.slice(0, mid));
+  node.rightNode = buildTree(arr.slice(mid + 1));
+  return node;
+};
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.rightNode !== null) {
+    prettyPrint(node.rightNode, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.leftNode !== null) {
+    prettyPrint(node.leftNode, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+function mergeSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+
+  let left = arr.splice(0, Math.round(arr.length / 2));
+  let right = arr;
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  let mergedArr = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      mergedArr.push(left[0]);
+      left.splice(0, 1);
+    } else {
+      mergedArr.push(right[0]);
+      right.splice(0, 1);
+    }
+  }
+
+  return [...mergedArr, ...left, ...right];
+}
+
+function removeSame(arr) {
+  for (i = 1; i < arr.length; i++) {
+    if (arr[i] === arr[i - 1]) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+  return arr;
+}
+
+const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+prettyPrint(tree.root);
